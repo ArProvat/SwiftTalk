879a88e657a82f2cf25d747c47cd5d0d7e698f96
Abdur Rahman <provatar0@gmail.com>
2024-11-13 00:56:28 +0600

    Set up tailwind react Router

---
+++ Client/SwiftTalk/src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className=' text-orange-500 text-2xl'>
+            hi this home 
+        </div>
+    );
+};
+
+export default Home;
